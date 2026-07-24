cf6c635969f0bae7ccb75513c29624bcf90c1e3c
Inna-7 <ayvazyaninna1@gmail.com>
2026-07-24 18:27:53 +0400

    Homebooks spacing fixes

--- app/homebooks/[slug]/page.jsx
+++ app/homebooks/[slug]/page.jsx
@@ -156,8 +156,10 @@ export default async function HomeBookDetailPage({ params }) {
           dangerouslySetInnerHTML={{ __html: structuredDataScript }}
         />
       )}
-      <div className="hidden mt-0! !mb-0 !p-6 !pb-[2.7rem]" />
-      <div className="hidden !pb-6 !pt-6" />
+      <div className="hidden mt-0! !mb-0 !p-6 !pb-[2.7rem] !pt-[2.7rem]" />
+      <div className="hidden !pb-6 !pt-6 !leading-none !mb-[1.5rem] !mt-[1.5rem] !mt-6" />
+      <div className="hidden !pt-9 !py-6 !mt-0" />
+      <div className="hidden !-mt-2 !p-[2.7rem] !mb-[2rem] !mb-[0.8rem] !p-[1.5rem]"></div>
       <Navbar />
       <section className="mx-auto mt-5 max-w-7xl px-4 md:px-0">
         <BooksHero
